8b638619313fa8c3a78d4fce6febc5b3a4214d61
RowanKinross <rowankinross@yahoo.co.uk>
2023-12-15 18:27:11 +0000

    prompts-and-confirms-added-with-more-pseudocode

--- script.js
+++ script.js
@@ -91,40 +91,83 @@ var upperCasedCharacters = [
 
 
 //Task:  Generate a password when the button is clicked
-    // 1. Present a series of prompts for password criteria
-      // - Length of password: at least 8 characters but no more than 128.
-        //prompt - how long would you like the password? (min 8 - max 128 characters)
-      // - Character types: Lowercase, Uppercase, Numeric, Special characters ($@%&*, etc)
-        //prompt - type all that apply 'L'for lowercase, 'U' for Uppercase, 'N' for Numbers, 'S' for Special Characters (in js use .toLowerCase)
-    // 2. Code should validate for each input and at least one character type should be selected
-    // 3. Once prompts are answered then the password should be generated and displayed in an alert or written to the page
+  // 1. Present a series of prompts for password criteria
+      //! - Length of password: at least 8 characters but no more than 128.
+      //!prompt - how long would you like the password? (min 8 - max 128 characters)
+          //!if number/input not within this range alert: the number you typed was not within this range, please try again.
+      //! - Character types: Lowercase, Uppercase, Numeric, Special characters ($@%&*, etc)
+        //! confirm do you at least 1 want lowercase letter?
+        //! confirm do you at least 1 want uppercase letter?
+        //! confirm do you at least 1 want number?
+        //! confirm do you at least 1 want special character? (  @ % + \\ / ' ! # $ ^ ? : , ) } { ] [ ~ - _ .   )
+  //! 2. Code should validate for each input and at least one character type should be selected
+        //! alert:if you don't select any character types, we can't generate you a password. Try again.
+  // 3. Once prompts are answered then the password should be generated and displayed in an alert or written to the page
+        // repeat arrays so that there is minimum 128 characters in each - make the new arrays into variable
+        // evenly distribute password length into the character types, add any remainder to lowercase
+        // make a random number generator
+        
+        // use random number generator to find "evenlyDistrubute" values to be used as indices in the subsequent arrays
+        // create new array with all values collected
+        //shuffle the array, then join - you should have your password
 
+
+
+        
+// Following variables need to be global
+// const charOptions = [];
+// const generatedPassword = []; 
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  
+  var passwordLength = parseInt(prompt('How log would you like your password? (min 8 - max 128 characters)'));
+  if (passwordLength > 7 && passwordLength < 129) {
+    alert(`You have chosen ${passwordLength} as your password length`)
+  } else {
+    alert(`input was not found within this range, please try again.`)
+    getPasswordOptions()
+  }
+  function getCharacterOptions() {
+    var confirmLowercase = confirm(`Would you like lowercase letters?`);
+    var confirmUppercase = confirm(`Would you like uppercase letters?`);
+    var confirmNumeric = confirm(`Would you like numbers?`);
+    var confirmSpecialChar = confirm(`Would you like special characters? (@%+\\/'!#$^?:,)}{][~-_.)`);
+    if (confirmLowercase === false
+      && confirmUppercase === false
+      && confirmNumeric === false
+      && confirmSpecialChar === false) {
+        alert(`at least one character type must be selected`)
+        getCharacterOptions()
+    } else {
+      alert(`Thank you, we are now generating your password...`)
+    }
+  }
+  getCharacterOptions()
 }
+getPasswordOptions();
+
+
 
 // Function for getting a random element from an array
-function getRandom(arr) {
+// function getRandom(arr) {
 
-}
+// }
 
-// Function to generate password with user input
-function generatePassword() {
+//  Function to generate password with user input
+// function generatePassword() {
 
-}
+// }
 
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+//  Get references to the #generate element
+// var generateBtn = document.querySelector('#generate');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+//  Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector('#password');
   
-  passwordText.value = password;
-}
+//   passwordText.value = password;
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+//  Add event listener to generate button
+// generateBtn.addEventListener('click', writePassword);
